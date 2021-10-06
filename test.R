caac01f3921ddd806f32632f00c39e5d8b8eee96
Marta Valor <mvalors@uoc.edu>
2021-10-06 20:56:24 +0200

    Prova copia inici PEC3

---
+++ test.R
@@ -0,0 +1,10 @@
+library(nlme)
+library(nlmeU)
+library(lme4)
+library(lattice)
+library(RLRsim)
+data(armd.wide, armd0, armd)
+set.seed(14)
+c<-sample(1:240,150)
+MVarmd<-armd.wide[c,]
+str(MVarmd)
